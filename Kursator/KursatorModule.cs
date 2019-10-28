0ef6f376b593ff01dc9d0ab55743189dd3c4f310
Karol Matyszewski <karolmatyszewski@gmail.com>
2019-10-28 19:59:58 +0100

    add autofac - core copleted

---
+++ Kursator/KursatorModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Autofac;
+using Domain.FixingDomain.Interfaces;
+using Kursator.Interfaces;
+using Kursator.Services;
+using Repositories.Repository;
+
+namespace Kursator
+{
+    public class KursatorModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<FixingService>().As<IFixingService>();
+            builder.RegisterType<NbpProvider>().As<INbpProvider>();
+            builder.Register(ctx =>
+            {
+                return new Uri("http://api.nbp.pl/api/");
+            }).As<Uri>();
+        }
+
+    }
+}
--- Kursator/Program.cs
+++ Kursator/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Autofac;
+using Kursator.Interfaces;
+using System;
 
 namespace Kursator
 {
     class Program
     {
+        private static IContainer Container { get; set; }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !Kursator!");
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<KursatorModule>();
+            Container = builder.Build();
+
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var service = scope.Resolve<IFixingService>();
+                var result = service.GetWorldwideFixings().Result;
+            }
         }
     }
 }
